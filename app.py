2aeb6f5e5380719beee38a1e9c1ab7604f78de24
fabriortenzi <fabriortenzi10@gmail.com>
2023-01-11 15:13:40 -0300

    test

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
-print("test")
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.security import check_password_hash, generate_password_hash
+
+# Configure application
+app = Flask(__name__)
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///budget.db")
+
+
