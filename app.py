8fbdbff8e243821ab41d031c732d027290884526
fabriortenzi <fabriortenzi10@gmail.com>
2023-01-12 17:51:57 -0300

    navbar and login

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 
+from helpers import login_required
+
 # Configure application
 app = Flask(__name__)
 
@@ -17,6 +19,29 @@ Session(app)
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///budget.db")
 
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
 @app.route("/")
+@login_required
 def index():
-    return render_template("layout.html")
+    return render_template("index.html")
+
+
+@app.route("/login")
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # GET
+    if request.method == "GET":
+        return render_template("login.html")
